cc962976cc6cec2f67c469d17b8de90838f85eb6
anilbhandari799 <55952821+anilbhandari799@users.noreply.github.com>
2019-10-06 22:00:18 +0545

    Add files via upload

---
+++ flot.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+
+ int a;
+ int b;
+  printf("enter the first number:");
+  scanf("%d",&a);
+
+  printf("enter the second number :");
+  scanf("%d" ,&b);
+
+  int c=a-b;
+  printf("the difference of two numbers is:%d" ,c);
+  getch();
+}
+
---
+++ looppp.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+main()
+{
+	
+int i,n;
+printf("\n symbolic pattern\n\n");
+for(i=1;i<=5;i++)
+{
+	for(n=1;n<=i;n++)
+	{
+		printf(" * ");
+	}
+	printf("\n");
+	}	
+	
+	
+	
+return 0;	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
